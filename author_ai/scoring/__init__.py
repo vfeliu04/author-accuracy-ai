f1c19e3768488bfff9620d8f10c493616aa6df3e
Vicente Feliu <vicentemariafeliu@gmail.com>
2025-10-28 23:48:31 +0100

    start fresh

--- author_ai/scoring/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Exports for Stage D scoring."""
-
-from author_ai.scoring.features import compute_features
-from author_ai.scoring.model import ScoringModel
-
-__all__ = ["compute_features", "ScoringModel"]
--- author_ai/ui/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Exports for Stage E rendering."""
-
-from author_ai.ui.render import ReportBuilder, RenderedReport
-
-__all__ = ["ReportBuilder", "RenderedReport"]
